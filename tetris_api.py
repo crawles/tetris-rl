0cffa73fb39bc780c5a40c3f1f781a3c2e4b01ef
crawles <crawles@gmail.com>
2017-10-12 13:48:08 -0700

    Working on solving blocks only

--- tetris_api.py
+++ tetris_api.py
@@ -10,7 +10,7 @@ class PyTetrisEnv():
     def __init__(self, reward_blocked_cells = False):
         self.game = None
 
-    def _process_reward(self, report, reward_factor=1):
+    def _process_reward(self, report, reward_factor=10):
         """Convert o to nparray, reward to float"""
         o, d, cum_score, reward = report.state,\
                                   report.done,\
@@ -31,7 +31,9 @@ class PyTetrisEnv():
         if d:
             reward = -1
 
-        #DUMB reward
-        reward = int(action in ['left', 'right'])
+#        #DUMB reward
+#        reward = int(action in ['left', 'right'])
+#        if not reward:
+#            reward = -1
 
         return o, reward, d, None
