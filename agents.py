54eac067f04fb36b381df27f725b8937705898ee
unknown <crawles@gmail.com>
2017-12-17 14:06:26 -0500

    Added agents.py; save functionality

---
+++ agents.py
@@ -0,0 +1,28 @@
+import json
+import os
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+import tetris_api
+import tf_rl_utils
+
+class FullyConnected(object):
+
+	def __init__(self, graph, X, y, n_units, learning_rate):
+		n_hidden1, n_hidden2, n_output = n_units
+		# 1st layer 
+		with graph.as_default() as g:
+			with tf.name_scope("dnn"):
+			    self.hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1",
+			                              activation=tf.nn.relu)
+			    self.hidden2 = tf.layers.dense(self.hidden1, n_hidden2, name="hidden2",
+			                              activation=tf.nn.relu)
+			    self.logits = tf.layers.dense(self.hidden2, n_output, name="output")
+			with tf.name_scope("loss"):
+				self.xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=self.logits)
+				self.loss = tf.reduce_mean(self.xentropy, name="loss")
+			with tf.name_scope("train"):
+				self.optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+				self.training_op = self.optimizer.minimize(self.loss)
